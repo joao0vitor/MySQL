1a4d2d8cdf8cc15eff447eef7571ec85c8917d34
JOAO VITOR DA SILVA SOUZA <joao.vssouza42@senacsp.edu.br>
2022-05-16 16:50:17 -0300

    Inicio do projeto banco de dados

---
+++ agenda.sql
@@ -0,0 +1,16 @@
+/**
+Agenda de contatos
+@author João Vitor
+*/
+
+-- Verificar bancos de dados
+show databases;
+
+-- Criar um novo banco de dados
+create database teste;
+create database agendajoao;
+-- Excluir o banco de dados
+drop database teste;
+
+-- Selecionar banco de dados
+use agendajoao;
